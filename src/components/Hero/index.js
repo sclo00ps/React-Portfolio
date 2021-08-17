c753ab237b1ded66ba439f9117efac8eb9520249
sclo00ps <louginescott@gmail.com>
2021-08-17 09:14:33 -0700

    added name to portfolio

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,6 +5,7 @@ import Image from "react-bootstrap/Image";
 function Hero() {
   return (
     <div className="heroPhoto">
+      <h1>Lougine Scott</h1>
       <Image className="hero" src="./portfolio_image_4.jpg" />;
     </div>
     // <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
